e4bcdebcc0b14676267721e527dbf3146acfd84c
Andrew Noorishad <anoorishad@me.com>
2022-10-17 12:49:11 -0700

    Add files via upload
    Initial Spring Files

---
+++ Summative-1-Andrew-Noorishad-John-Netzel/src/main/java/com/game_store/Summative1AndrewNoorishadJohnNetzel/Summative1AndrewNoorishadJohnNetzelApplication.java
@@ -0,0 +1,13 @@
+package com.game_store.Summative1AndrewNoorishadJohnNetzel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative1AndrewNoorishadJohnNetzelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative1AndrewNoorishadJohnNetzelApplication.class, args);
+	}
+
+}
---
+++ Summative-1-Andrew-Noorishad-John-Netzel/src/test/java/com/game_store/Summative1AndrewNoorishadJohnNetzel/Summative1AndrewNoorishadJohnNetzelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game_store.Summative1AndrewNoorishadJohnNetzel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative1AndrewNoorishadJohnNetzelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
