0a36152a81ac9ece1adced398b2e9775676e09a8
anoorishad <anoorishad@me.com>
2022-10-18 09:01:22 -0700

    Added console controller.

---
+++ Summative-1-Andrew-Noorishad-John-Netzel/src/main/java/com/game_store/Summative1AndrewNoorishadJohnNetzel/repository/ConsoleRepository.java
@@ -0,0 +1,7 @@
+package com.game_store.Summative1AndrewNoorishadJohnNetzel.repository;
+
+import com.game_store.Summative1AndrewNoorishadJohnNetzel.model.Console;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsoleRepository extends JpaRepository<Console, Integer> {
+}
