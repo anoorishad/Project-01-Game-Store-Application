ae5b6a9887e34a95a18eda48c795da140221f707
anoorishad <anoorishad@me.com>
2022-10-18 08:19:12 -0700

    Added models

---
+++ Summative-1-Andrew-Noorishad-John-Netzel/src/main/java/com/game_store/Summative1AndrewNoorishadJohnNetzel/model/ProcessingFee.java
@@ -0,0 +1,57 @@
+package com.game_store.Summative1AndrewNoorishadJohnNetzel.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "processing_fee")
+public class ProcessingFee {
+
+    @Column(name = "product_type")
+    private String productType;
+
+    private BigDecimal fee;
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public BigDecimal getFee() {
+        return fee;
+    }
+
+    public void setFee(BigDecimal fee) {
+        this.fee = fee;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProcessingFee that = (ProcessingFee) o;
+        return Objects.equals(productType, that.productType) && Objects.equals(fee, that.fee);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productType, fee);
+    }
+
+    @Override
+    public String toString() {
+        return "ProcessingFee{" +
+                "productType='" + productType + '\'' +
+                ", fee=" + fee +
+                '}';
+    }
+}
