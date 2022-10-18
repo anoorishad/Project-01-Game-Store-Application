4b57f69846f084071ea996fc9ca416406bcb5ada
anoorishad <anoorishad@me.com>
2022-10-18 14:02:32 -0700

    Added source of CorsConfig code

--- Summative-1-Andrew-Noorishad-John-Netzel/src/main/java/com/game_store/Summative1AndrewNoorishadJohnNetzel/CorsConfig.java
+++ Summative-1-Andrew-Noorishad-John-Netzel/src/main/java/com/game_store/Summative1AndrewNoorishadJohnNetzel/CorsConfig.java
@@ -5,6 +5,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+// Source class record store activity
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
