e19d5455c72c82ee00c3776135647b2236c7afbb
John Netzel <johnrnetzel@gmail.com>
2022-10-18 09:09:27 -0800

    Add state and event listeners to GameForm

--- summative-1-front-end/src/components/GameForm.js
+++ summative-1-front-end/src/components/GameForm.js
@@ -1,17 +1,38 @@
+import { useState } from "react";
+
 function GameForm(props) {
-    return <form>
-        <label htmlFor="title-input">Title</label>
-        <input name="title-input"></input>
-        <label htmlFor="esrb-rating-input">ESRB Rating</label>
-        <input name="esrb-rating-input"></input>
-        <label htmlFor="description-input">Description</label>
-        <input name="description-input"></input>
-        <label htmlFor="price-input">Price</label>
-        <input name="price-input"></input>
-        <label htmlFor="studio-input">Studio</label>
-        <input name="studio-input"></input>
-        <label htmlFor="quantity-input">Quantity</label>
-        <input name="quantity-input"></input>
+    const [title, setTitle] = useState("");
+    const [esrbRating, setEsrbRating] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState("");
+    const [studio, setStudio] = useState("");
+    const [quantity, setQuantity] = useState("");
+
+    return <form onSubmit={() => alert(title)}>
+        <div>
+            <label htmlFor="title-input">Title</label>
+            <input name="title-input" onChange={(e) => setTitle(e.target.value)}></input>
+        </div>
+        <div>
+            <label htmlFor="esrb-rating-input">ESRB Rating</label>
+            <input name="esrb-rating-input" onChange={(e) => setEsrbRating(e.target.value)}></input>
+        </div>
+        <div>
+            <label htmlFor="description-input">Description</label>
+            <input name="description-input" onChange={(e) => setDescription(e.target.value)}></input>
+        </div>
+        <div>
+            <label htmlFor="price-input">Price</label>
+            <input name="price-input" onChange={(e) => setPrice(e.target.value)}></input>
+        </div>
+        <div>
+            <label htmlFor="studio-input">Studio</label>
+            <input name="studio-input" onChange={(e) => setStudio(e.target.value)}></input>
+        </div>
+        <div>
+            <label htmlFor="quantity-input">Quantity</label>
+            <input name="quantity-input" onChange={(e) => setQuantity(e.target.value)}></input>
+        </div>
         <input type="submit"></input>
     </form>
 }
