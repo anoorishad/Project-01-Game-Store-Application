9ce1a7adbc73937f6b8ff4329970d5727d9f32f2
John Netzel <johnrnetzel@gmail.com>
2022-10-18 06:35:45 -0800

    Create GameTable component

--- summative-1-front-end/src/App.js
+++ summative-1-front-end/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import GameTable from "./components/GameTable"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Summative Project 1 Front End</h1>
       </header>
+      <main>
+        <GameTable></GameTable>
+      </main>
     </div>
   );
 }
---
+++ summative-1-front-end/src/components/GameTable.js
@@ -0,0 +1,5 @@
+function GameTable(props) {
+    return <div>I'm a table!</div>
+}
+
+export default GameTable;
