94f6f3f699439b28805184e7d2be819663d7f7a1
John Netzel <johnrnetzel@gmail.com>
2022-10-18 06:48:59 -0800

    Add column headers and test data to GameTable

--- summative-1-front-end/src/components/GameTable.js
+++ summative-1-front-end/src/components/GameTable.js
@@ -1,5 +1,31 @@
 function GameTable(props) {
-    return <div>I'm a table!</div>
+    return <>
+        <h2>Games</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>ESRB Rating</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Studio</th>
+                    <th>Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>The Bestest Game EVAR!!!</td>
+                    <td>M</td>
+                    <td>The only game you'll ever need to play. Seriously. Why are you still reading this? Go buy it already!</td>
+                    <td>69.99</td>
+                    <td>AwesomeSoft Studios</td>
+                    <td>2</td>
+                </tr>
+            </tbody>
+        </table>
+    </>
 }
 
 export default GameTable;
