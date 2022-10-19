0a6a1d121a1eac0208fdb91e6f8ab14b9cce23f6
John Netzel <johnrnetzel@gmail.com>
2022-10-19 08:16:44 -0800

    Merge service layers into single class

--- Summative-1-Andrew-Noorishad-John-Netzel/src/main/java/com/game_store/Summative1AndrewNoorishadJohnNetzel/service/ServiceLayer.java
+++ Summative-1-Andrew-Noorishad-John-Netzel/src/main/java/com/game_store/Summative1AndrewNoorishadJohnNetzel/service/ServiceLayer.java
@@ -1,7 +1,9 @@
 package com.game_store.Summative1AndrewNoorishadJohnNetzel.service;
 
 import com.game_store.Summative1AndrewNoorishadJohnNetzel.model.Invoice;
+import com.game_store.Summative1AndrewNoorishadJohnNetzel.model.ProcessingFee;
 import com.game_store.Summative1AndrewNoorishadJohnNetzel.model.SalesTaxRate;
+import com.game_store.Summative1AndrewNoorishadJohnNetzel.repository.ProcessingFeeRepository;
 import com.game_store.Summative1AndrewNoorishadJohnNetzel.repository.TaxRateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,16 +13,18 @@ import java.math.BigDecimal;
 import java.util.Optional;
 
 @Service
-public class SalesTaxServiceLayer {
+@Transactional
+public class ServiceLayer {
     private TaxRateRepository taxRateRepository;
+    private ProcessingFeeRepository processingFeeRepository;
 
     @Autowired
-    public SalesTaxServiceLayer(TaxRateRepository taxRateRepository) {
+    public ServiceLayer(TaxRateRepository taxRateRepository, ProcessingFeeRepository processingFeeRepository) {
         this.taxRateRepository = taxRateRepository;
+        this.processingFeeRepository = processingFeeRepository;
     }
 
-    @Transactional
-    public Invoice calculateSalesTax(Invoice invoice) {
+    public void calculateSalesTax(Invoice invoice) {
         String state = invoice.getState(); // Read state from invoice
         Optional<SalesTaxRate> taxRateRecord = taxRateRepository.findById(state); // Query DB for sales tax by state name
         if(!taxRateRecord.isPresent()) { // If no sales tax record is found...
@@ -29,6 +33,19 @@ public class SalesTaxServiceLayer {
         BigDecimal taxRate = taxRateRecord.get().getRate(); // Read sales tax rate from record from sales tax table
         BigDecimal tax = invoice.getSubtotal().multiply(taxRate);
         invoice.setTax(tax); // Load value into invoice
-        return invoice;
+    }
+
+    public void calculateProcessingFee(Invoice invoice) {
+        String productType = invoice.getItemType();
+        Optional<ProcessingFee> processingFeeRecord = processingFeeRepository.findById(productType);
+        if(!processingFeeRecord.isPresent()) {
+            throw new RuntimeException("Cannot find processing for state: " + productType);
+        }
+        BigDecimal processingFee = processingFeeRecord.get().getFee();
+        // Check for large (>10) orders
+        if(invoice.getQuantity() > 10) {
+            processingFee = processingFee.add(BigDecimal.valueOf(15.49));
+        }
+        invoice.setProcessingFee(processingFee); // Load value into invoice
     }
 }
